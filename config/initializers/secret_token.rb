130ad03d258907c3e99087d3961675a81214582b
Je <jetapahob@gmail.com>
2013-02-22 00:31:17 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Applive::Application.config.secret_token = '3dde97b83ba3e1c6951149e5bcc48fdbfaa80281b292318e1183025402f8b41d3a7c538750637f1608f8583b36ec4885acf7ceca717c5e09f8f9a48cc06a12f2'
